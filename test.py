9fca4206e5ea0f07d49fd453f5f02501106a2d02
Nikhil Ramjuttun <40278491+NRamjuttun1@users.noreply.github.com>
2020-02-25 18:55:09 +0000

    Test works but 26 == BA which should be AA

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
-x = 569
+x = 26
 sstring = ""
-numsymbols = (x//26) + 1
-for i in range(numsymbols):
-    count = 0
-    power = numsymbols - 1 - i
-    temp = x
-    count = temp//26**power
-    temp = temp & (26**power)
+numsymbols = 1
+i = 0
+temp = x
+while(x >= 26**numsymbols):
+    numsymbols += 1
+for i in range(1, numsymbols + 1):
+    power = numsymbols - i
+    print("Power is {}".format(power))
+    print("Temp is {}".format(temp))
+    count = temp//(26**power)
+    print("Count is {}".format(count))
+    temp = temp%(26**power)
+    print("Temp is now {}".format(temp))
     count = chr(65+count)
+    print("New Count is now {}".format(count))
     sstring += count
 print(sstring)
